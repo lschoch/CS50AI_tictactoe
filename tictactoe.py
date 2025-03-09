b4f9b6761b270547a30e388f3bc5370fe328731b
lschoch <lschoch@gmail.com>
2025-03-09 03:19:27 +0000

    Sat, Mar 8, 2025, 10:19 PM -05:00

--- tictactoe.py
+++ tictactoe.py
@@ -225,7 +225,7 @@ def get_o_action(board):
         impwin = impending_winner(res)
         if impwin:
             return impwin
-        lst.append((action, min_value(res)))
+        lst.append((action, max_value(res)))
     optimal_action = lst[0][0]
     for i in range(1, len(lst)):
         if player(board) == X:
